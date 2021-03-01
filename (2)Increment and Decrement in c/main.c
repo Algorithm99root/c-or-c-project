60b287ddf9d91454a40f2ba0d48f24383615aa3d
Ashik <ar3229454@gmail.com>
2021-03-02 02:17:49 +0600

    (2)Increment and Decrement in c

---
+++ (2)Increment and Decrement in c/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+
+    // Increment and Decrement program
+
+    int a = 9;
+    printf("%d ,%d, %d, %d, %d, %d, %d", ++a, --a, a, a, a++, --a, ++a);
+
+    return 0;
+}
