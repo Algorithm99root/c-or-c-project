c81fe17660bf776c2e1408a51700e8a035647ce6
Ashik <ar3229454@gmail.com>
2021-04-04 01:13:56 +0600

    String problem in C program

---
+++ String in C program/problem_string_3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// String problem In C program
+
+int main()
+{
+    char *c = "GATECSIT2017";
+    char *p = c;
+    printf("%d", (int)strlen(c + 2 [p] - 6 [p] - 1));
+    printf("\n");
+    printf("\n");
+    system("pause");
+    return 0;
+}
