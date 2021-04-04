b80876bfbcc53618805042b6fd728b2957ccdfb6
Ashik <ar3229454@gmail.com>
2021-04-04 12:20:40 +0600

    typedef keyword-2 in C program

---
+++ Structure in C program/typedef_keyword_2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int hack;
+
+int main()
+{
+    hack var = 100;
+    printf("%d", var);
+    printf("\n");
+    printf("\n");
+    system("pause");
+    return 0;
+}
