4430f9d3016a96a4bdb0a0d286b218530ca0c668
Ashik <ar3229454@gmail.com>
2021-04-06 14:40:13 +0600

    Enumeration in C program

---
+++ Enumeration in C program/Enumeration_example.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Enumeration In C program
+
+enum point
+{
+    x = 0,
+    y = 1,
+    z
+};
+
+int main()
+{
+
+    printf("%d %d %d ", x, y, z);
+    printf("\n");
+    printf("\n");
+    system("pause");
+    return 0;
+}
